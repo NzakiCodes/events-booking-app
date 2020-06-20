edf87a5b0a142f102c5dbb7722269c8a3bcc9ce8
NzakiCodes <ekisnzaki@gmail.com>
2020-06-20 22:08:08 +0100

    Updated app.js Refectored Code

--- app.js
+++ app.js
@@ -1,174 +1,26 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const bodyParser = require("body-parser");
 
-const Event = require('./models/events');
-const User = require('./models/users');
-
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
 
 const app = express();
+
 app.use(bodyParser.json());
-
-const events = eventsIds => {
-  return Event.find({ _id: { $in: eventsIds } })
-    .then(events => {
-      return events.map(event => {
-        return {
-          ...event._doc,
-          _id: event.id,
-          creator: user.bind(this, event.creator)
-        }
-      })
-    }).catch(err => { throw err; })
-};
-
-const user = userId => {
-  return User.findById(userId)
-    .then(user => {
-      return {
-        ...user._doc,
-        _id: user.id,
-        createdEvents: events.bind(this, user._doc.createdEvents)
-      };
-    })
-    .catch(err => {
-      throw err;
-    });
-};
-
 app.use('/graphql', graphqlHTTP({
-  schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-            creator: User!
-        }
-
-        type User {
-          _id: ID!
-          email: String!
-          password: String
-          createdEvents: [Event!]
-        }
-
-        input EvenInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-          email: String!
-          password: String!
-        }
-
-        type RootQuery {
-            events:[Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EvenInput): Event
-            createUser(userInput: UserInput): User
-        }
-
-        schema {
-          query: RootQuery
-          mutation: RootMutation
-        }
-    `),
-  rootValue: {
-    events: () => {
-      return Event
-        .find()
-        .then(events => {
-          return events.map(event => {
-            return {
-              ...event._doc,
-              _id: event._doc._id,
-              creator: user.bind(this, event._doc.creator)
-            };
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          throw err;
-        })
-    },
-    createEvent: (args) => {
-      const event = new Event({
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: new Date(args.eventInput.date),
-        creator: '5eedad67bbc87f1d8cab7d24'
-      });
-      let createdEvent;
-      return event
-        .save()
-        .then(result => {
-          createdEvent = {
-            ...result._doc,
-            _id: result._doc._id.toString(),
-            creator: user.bind(this, result._doc.creator)
-          };
-          return User.findById('5eedad67bbc87f1d8cab7d24')
-
-        })
-        .then(user => {
-          if (!user) {
-            throw new Error('User not found')
-          }
-          user.createdEvents.push(event);
-          return user.save();
-        })
-        .then(result => {
-          return createdEvent;
-        })
-        .catch(err => {
-          console.log(err);
-          throw err;
-        });
-      return event;
-    },
-    createUser: args => {
-      return User.findOne({ email: args.userInput.email })
-        .then(user => {
-          if (user) {
-            throw new Error('User exits already')
-          }
-          return bcrypt
-            .hash(args.userInput.password, 12)
-            .then(hashedPassword => {
-              const user = new User({
-                email: args.userInput.email,
-                password: hashedPassword
-              });
-              return user.save();
-            })
-            .then(result => {
-              return { ...result._doc, password: null, _id: result.id }
-            })
-            .catch(err => {
-              throw err;
-            })
-        });
-
-    }
-  },
+  schema: graphQlSchema,
+  rootValue: graphQlResolvers,
   graphiql: true
 }))
 
-mongoose.connect(`mongodb://localhost/${process.env.MONGODB_DATABASE}`).then(() => {
-  app.listen(3000, () => {
-    console.log('App listening on port 3000')
+try {
+  mongoose.connect(`mongodb://localhost/${process.env.MONGODB_DATABASE}`).then(() => {
+    app.listen(3000, () => {
+      console.log('App listening on port 3000')
+    })
   })
-}).catch(err => {
+} catch (err) {
   console.log(err);
-})
+}
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,110 @@
+const bcrypt = require('bcryptjs');
+
+const Event = require('../..//models/events');
+const User = require('../../models/users');
+
+const events = eventsIds => {
+    return Event.find({ _id: { $in: eventsIds } })
+      .then(events => {
+        return events.map(event => {
+          return {
+            ...event._doc,
+            _id: event.id,
+            date: new Date(event.date).toISOString(),
+            creator: user.bind(this, event.creator)
+          }
+        })
+      }).catch(err => { throw err; })
+  };
+  
+  const user = async userId => {
+    try {
+          const user = await User.findById(userId);
+          return {
+              ...user._doc,
+              _id: user.id.toString(),
+              createdEvents: events.bind(this, user._doc.createdEvents)
+          };
+      }
+      catch (err) {
+          throw err;
+      }
+  };
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event
+                .find();
+            return events.map(event => {
+                return {
+                    ...event._doc,
+                    _id: event._doc._id.toString(),
+                    date: new Date(event.date).toISOString(),
+                    creator: user.bind(this, event._doc.creator)
+                };
+            });
+        }
+        catch (err) {
+            console.log(err);
+            throw err;
+        }
+    },
+    createEvent: (args) => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: '5eedad67bbc87f1d8cab7d24'
+        });
+        let createdEvent;
+        return event
+            .save()
+            .then(result => {
+                createdEvent = {
+                    ...result._doc,
+                    _id: result._doc._id.toString(),
+                    date: new Date(result.date).toISOString(),
+                    creator: user.bind(this, result._doc.creator)
+                };
+                return User.findById('5eedad67bbc87f1d8cab7d24')
+
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error('User not found')
+                }
+                user.createdEvents.push(event);
+                return user.save();
+            })
+            .then(result => {
+                return createdEvent;
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
+        return event;
+    },
+    createUser: async args => {
+        const user = await User.findOne({ email: args.userInput.email });
+        if (user) {
+            throw new Error('User exits already');
+        }
+        try {
+            const hashedPassword = await bcrypt
+                .hash(args.userInput.password, 12);
+            const user_1 = new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            });
+            const result = await user_1.save();
+            return { ...result._doc, password: null, _id: result.id };
+        }
+        catch (err) {
+            throw err;
+        }
+
+    }
+};
