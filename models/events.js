72ca9e62606815a61ae768df7410b0ad4f699001
NzakiCodes <ekisnzaki@gmail.com>
2020-06-20 21:35:34 +0100

    Updated all files

--- models/events.js
+++ models/events.js
@@ -3,22 +3,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = Schema({
-  title:{
-    type:String,
-    required:true
+  title: {
+    type: String,
+    required: true
   },
-  description:{
-    type:String,
-    required:true
+  description: {
+    type: String,
+    required: true
   },
-  price:{
-    type:Number,
-    required:true
+  price: {
+    type: Number,
+    required: true
   },
-  date:{
-    type:Date,
-    required:true
+  date: {
+    type: Date,
+    required: true
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }
-});
+}); 
 
-module.exports = mongoose.model('Event',eventSchema);
+module.exports = mongoose.model('Event', eventSchema);
