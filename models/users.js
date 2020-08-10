08f03eac06ff4eb7977a8edcc421b9e708f47c30
NzakiCodes <ekisnzaki@gmail.com>
2020-08-10 21:15:20 +0100

    Update users.js

--- models/users.js
+++ models/users.js
@@ -5,16 +5,16 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true
-  } ,
+  },
   password: {
     type: String,
     required: true
   },
   createdEvents: [
-      {
-          type: Schema.Types.ObjectId,
-          ref: 'Event'
-      }
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Event'
+    }
   ]
 });
 
